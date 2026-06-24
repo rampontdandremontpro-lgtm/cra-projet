5c66696e5bf818d8e6b86cf2cdb4d76878b41a2c
rampont <rampontdandremontd@gmail.com>
2026-06-24 15:24:25 -0400

    front/back.v3.5

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -8,56 +8,70 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 class AssignClientsDto {
   clientIds: number[];
 }
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles('ADMIN')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @Roles('ADMIN', 'RH')
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Roles('ADMIN', 'RH')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(Number(id));
   }
 
+  @Roles('ADMIN')
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(Number(id), updateUserDto);
   }
 
+  @Roles('ADMIN', 'RH')
   @Get(':id/clients')
-getAssignedClients(@Param('id') id: string) {
-  return this.usersService.getAssignedClients(Number(id));
-}
+  getAssignedClients(@Param('id') id: string) {
+    return this.usersService.getAssignedClients(Number(id));
+  }
 
-@Post(':id/clients')
-assignClients(
-  @Param('id') id: string,
-  @Body() body: AssignClientsDto,
-) {
-  return this.usersService.assignClients(
-    Number(id),
-    body.clientIds,
-  );
-}
+  @Roles('ADMIN', 'RH')
+  @Post(':id/clients')
+  assignClients(
+    @Param('id') id: string,
+    @Body() body: AssignClientsDto,
+  ) {
+    return this.usersService.assignClients(
+      Number(id),
+      body.clientIds,
+    );
+  }
 
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(Number(id));
